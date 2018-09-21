685c05f3c0bea7d30ad8ddaf2f93d91adbbae066
Tianhao <zhangth1990@hotmail.com>
2018-09-20 22:58:09 -0600

    Download shopify JSON data to android

--- app/src/main/java/com/example/tianhao/shopifychallengetianhaozhang/MainActivity.java
+++ app/src/main/java/com/example/tianhao/shopifychallengetianhaozhang/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -12,39 +13,41 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    public class DonloadTask extends AsyncTask<String,Void,String>{
+    public class DownloadTask extends AsyncTask<String,Void,String>{
+
 
         @Override
         protected String doInBackground(String... urls) {
             String result = "";
             URL url;
             HttpURLConnection urlConnection = null;
-
-            try{
+            try {
                 url = new URL(urls[0]);
                 urlConnection = (HttpURLConnection) url.openConnection();
-                InputStream in = urlConnection.getErrorStream();
+                InputStream in = urlConnection.getInputStream();
                 InputStreamReader reader = new InputStreamReader(in);
                 int data = reader.read();
 
-                while(data != -1){
-                    char current = (char)data;
+                while (data != -1) {
+                    char current = (char) data;
                     result += current;
                     data = reader.read();
                 }
-                return result;
 
-            }catch (Exception e){
+                return result;
+            } catch (Exception e) {
                 e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "Could not find weather :(", Toast.LENGTH_SHORT).show();
                 return null;
             }
+
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             Log.i("JSON",s);
-            
+
         }
     }
 
@@ -52,5 +55,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        task.execute("https://shopicruit.myshopify.com/admin/products.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6");
+
     }
 }
